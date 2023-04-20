576ea7b5058f800e70cc6adb6d0af5e775a2bd5f
AdityaSidham <124783092+AdityaSidham@users.noreply.github.com>
2023-04-21 00:36:37 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String args[]) {
-        int n1 = 225, n2 = 115, sum;
+        int n1 = 100, n2 = 500, sum;
         sum = n1 + n2;
         System.out.println("The sum of numbers is: " + sum);
     }
